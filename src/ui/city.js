3c438a119efd334edd7f1b52952d02f89d18fa6e
chris <wfsr@foxmail.com>
2013-05-18 01:35:50 +0800

    fixed for issues #7

--- src/ui/city.js
+++ src/ui/city.js
@@ -45,7 +45,6 @@ define(function (require) {
          * @property {boolean} disabled 控件的不可用状态
          * @property {string|HTMLElement} main 控件渲染容器
          * @property {string} prefix 控件class前缀，同时将作为main的class之一
-         * @property {string} hintText 控件标题区域显示的提示文字
          * @property {number} index 默认激活的标签索引
          * @property {string} activeClass 激活标签、内容的class
          * @property {boolean} autoFill 是否自动填充默认城市数据(机票可用城市数据)
@@ -61,9 +60,6 @@ define(function (require) {
 
             // 控件class前缀，同时将作为main的class之一
             prefix: 'ecl-hotel-ui-city',
-            
-            // 控件标题区域显示的提示文字
-            hintText: '(可直接输入城市或城市拼音)',
 
             // 默认激活的标签索引
             index: 0,
@@ -193,9 +189,6 @@ define(function (require) {
             var labels  = [];
             var panels  = [];
 
-            labels.push('<span>热门</span>城市' + options.hintText);
-            labels.push('<a href="#" class="' + prefix + '-close">X</a>');
-
             labels.push('<ul class="' + prefix + '-labels c-clearfix">');
             panels.push('<ul class="' + prefix + '-panels">');
 
@@ -271,10 +264,8 @@ define(function (require) {
 
                 if (index) {
                     this.change(index);
-
-                    var text = el.innerHTML;
-                    this.hinter.innerHTML = (index === '0' ? '' : '拼音') + text;
                 }
+
                 break;
 
             default:
@@ -319,7 +310,6 @@ define(function (require) {
                 var list    = main.getElementsByTagName('ul');
                 this.labels = list[0].getElementsByTagName('li');
                 this.panels = list[1].getElementsByTagName('li');
-                this.hinter = main.getElementsByTagName('span')[0];
             }
         },
 
