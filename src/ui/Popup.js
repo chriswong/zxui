31983be893810a2d85b538892c09135d8ad3ccd5
chris <wfsr@foxmail.com>
2013-06-04 23:35:07 +0800

    文件名大小写问题 step3

---
+++ src/ui/Popup.js
@@ -0,0 +1,474 @@
+/**
+ * ZXUI (Zhixin UI)
+ * Copyright 2013 Baidu Inc. All rights reserved.
+ * 
+ * @file  弹出层
+ * @author  chris(wfsr@foxmail.com)
+ */
+
+
+define(function (require) {
+
+    var T = baidu;
+    var DOM = T.dom;
+    var PAGE = T.page;
+    var Control = require('./Control');
+
+    /**
+     * 弹出层控件
+     * 
+     * @constructor
+     * @extends module:Control
+     * @requires Control
+     * @exports Popup
+     * @see module:City
+     * @see module:Calendar
+     */
+    var Popup = function () {
+        this.constructor.superClass.constructor.apply(this, arguments);
+    };
+
+    Popup.prototype = {
+
+        /**
+         * 控件类型标识
+         * 
+         * @private
+         * @type {string}
+         */
+        type: 'Popup',
+
+        /**
+         * 控件配置项
+         * 
+         * @private
+         * @name module:Popup#options
+         * @type {Object}
+         * @property {boolean} disabled 控件的不可用状态
+         * @property {(string | HTMLElement)} main 控件渲染容器
+         * @property {(string | HTMLElement)} target 计算弹出层相对位置的目标对象
+         * @property {string} triggers 触发显示弹出层的节点
+         * @property {string} content 提示的内容信息
+         * @property {string} dir 弹出层相对 target 的位置，支持8个方向
+         * 可选值（默认为 bl）：
+         * tr | rt | rb | br | bl | lb | lt | tl | tc | rc | bc | lc
+         * 也可通过在 triggers 上设置 data-popup来指定
+         * @property {string} prefix 控件class前缀，同时将作为main的class之一
+         * @property {Object.<string, number>} offset 弹出层显示的偏移量
+         * @property {number} offset.x x 轴方向偏移量
+         * @property {number} offset.y y轴方向偏移量
+         */
+        options: {
+
+            /**
+             * 提示框的不可用状态，默认为false。处于不可用状态的提示框不会出现。
+             * 
+             * @type {boolean}
+             * @default
+             */
+            disabled: false,
+
+            /**
+             * 控件渲染主容器
+             * 
+             * @type {(string | HTMLElement)}
+             */
+            main: '',
+
+            /**
+             * 计算弹出层相对位置的目标对象
+             * 
+             * @type {(string | HTMLElement)}
+             */
+            target: '',
+
+            /**
+             * 触发显示弹出层的节点class
+             * 
+             * @type {string}
+             */
+            triggers: '',
+
+            /**
+             * 显示的内容
+             * 
+             * @type {string}
+             */
+            content: '',
+
+            /**
+             * 弹出层显示在 trigger 的相对位置
+             * 
+             * 可选值：tr | rt | rb | br | bl | lb | lt | tl | tc | rc | bc | lc
+             * 也可通过在 triggers 上设置 data-popup来指定
+             * 
+             * @type {string}
+             * @defaultvalue
+             */ 
+            dir: 'bl',
+
+            /**
+             * 控件class前缀，同时将作为main的class之一
+             * 
+             * @type {string}
+             * @defaultvalue
+             */
+            prefix: 'ecl-hotel-ui-popup',
+
+            /**
+             * 浮层显示的偏移量
+             * 
+             * @type {string}
+             */
+            offset: {
+
+                /**
+                 * x 轴方向偏移量
+                 * 
+                 * @type {number}
+                 * @defaultvalue
+                 */
+                x: 0,
+
+                /**
+                 * y 轴方向偏移量
+                 * 
+                 * @type {string}
+                 * @defaultvalue
+                 */
+                y: 0
+            }
+        },
+
+
+        /**
+         * 需要绑定 this 的方法名，多个方法以半角逗号分开
+         * 
+         * @private
+         * @type {string}
+         */
+        binds: 'onResize, onShow, onHide',
+
+        /**
+         * 控件初始化
+         * 
+         * @private
+         * @params {Object} options 配置项
+         * @see module:Popup#options
+         */
+        init: function (options) {
+            options = this.setOptions(options);
+
+            this.disabled  = options.disabled;
+            this.content   = options.content;
+
+            if (options.target) {
+                this.target = T.g(options.target);                
+            }
+
+            var prefix = options.prefix;
+            var main   = this.main = options.main 
+                && T.g(options.main)
+                || document.createElement('div');
+
+            if (options.main) {
+                T.addClass(main, prefix);
+            }
+            else {
+                main.className  = prefix;
+                main.style.left = '-2000px';
+            }
+
+            var me       = this;
+            var triggers = options.triggers;
+
+            if (T.isString(triggers)) {
+                triggers = T.q(options.triggers);
+            }
+
+            T.each(
+                triggers,
+                function (trigger) {
+                    T.on(trigger, 'click', me.onShow);
+                }
+            );
+
+            this.triggers = triggers;
+        },
+
+        /**
+         * 绘制控件
+         * 
+         * @public
+         * @fires module:Popup#click 点击事件
+         * @return {module:Popup} 当前实例
+         */
+        render: function () {
+            var main = this.main;
+
+            if (!this.rendered) {
+                this.rendered = true;
+                !this.options.main && document.body.appendChild(main);
+
+                var me = this;
+
+                T.on(
+                    main,
+                    'click',
+                    function (e) {
+
+                        /**
+                         * @event module:Popup#click
+                         * @type {Object}
+                         * @property {DOMEvent} event 事件源对象
+                         */
+                        me.fire('click', { event: e });
+                    }
+                );
+
+            }
+
+            if (this.content) {
+                main.innerHTML = this.content;
+            }
+
+            return this;
+        },
+
+        /**
+         * 浏览器可视尺寸改变时处理
+         * 
+         * @private
+         */
+        onResize: function () {
+            clearTimeout(this.resizeTimer);
+
+            var me = this;
+
+            this.resizeTimer = setTimeout(function () {
+                me.show();
+            }, 100);
+        },
+
+        /**
+         * 显示浮层前处理
+         * 
+         * @private
+         * @param {DOMEvent} e DOM 事件对象
+         * @fires module:Popup#beforeShow 显示前事件
+         */
+        onShow: function (e) {
+
+            var oldTarget = this.target;
+
+            /**
+             * @event module:Popup#beforeShow
+             * @type {Object}
+             * @property {DOMEvent} event 事件源对象
+             */
+            this.fire('beforeShow', { event: e });
+
+            if (oldTarget !== this.target) {
+                this.hide();
+            }
+
+            this.show();
+
+            var me = this;
+            this.trigger = T.event.getTarget(e);
+
+            this.timer = setTimeout(function () {
+                T.on(document, 'click', me.onHide);
+                T.on(window, 'resize', me.onResize);
+            }, 0);
+        },
+
+        /**
+         * 隐藏浮层前处理
+         * 
+         * @private
+         */
+        onHide: function (e) {
+            var target = T.event.getTarget(e);
+            var main   = this.main;
+
+            if (main === target || DOM.contains(main, target)) {
+                return;
+            }
+
+            this.hide();
+
+            clearTimeout(this.resizeTimer);
+        },
+
+        /**
+         * 显示浮层
+         * 
+         * @public
+         * @fires module:Popup#show 显示事件
+         */
+        show: function () {
+            this.computePosition();
+
+            /**
+             * @event module:Popup#show
+             */
+            this.fire('show');
+        },
+
+        /**
+         * 隐藏浮层
+         * 
+         * @public
+         * @fires module:Popup#hide 隐藏事件
+         */
+        hide: function () {
+            this.main.style.left = '-2000px';
+
+            /**
+             * @event module:Popup#hide
+             */
+            this.fire('hide');
+
+            T.un(document, 'click', this.onHide);
+            T.un(window, 'resize', this.onResize);
+
+            clearTimeout(this.timer);
+            clearTimeout(this.resizeTimer);
+        },
+
+        /**
+         * 计算浮层及箭头显示位置
+         * 
+         * @private
+         */
+        computePosition: function () {
+            var options      = this.options;
+            var target       = this.target || this.triggers[0];
+            var main         = this.main;
+            var dir          = options.dir;
+            var position     = DOM.getPosition(target);
+
+            // 目标的8个关键坐标点
+            var top          = position.top;
+            var left         = position.left;
+            var width        = target.offsetWidth;
+            var height       = target.offsetHeight;
+            var right        = left + width;
+            var bottom       = top + height;
+            var center       = left + (width / 2);
+            var middle       = top + (height / 2);
+
+            // 提示层宽高
+            var mainWidth    = main.offsetWidth;
+            var mainHeight   = main.offsetHeight;
+
+            // 视窗范围
+            var scrollTop    = PAGE.getScrollTop();
+            var scrollLeft   = PAGE.getScrollLeft();
+            var scrollRight  = scrollLeft + PAGE.getViewWidth();
+            var scrollBottom = scrollTop + PAGE.getViewHeight();
+
+            // 属性配置优于实例配置
+            var dirFromAttr = target.getAttribute('data-popup');
+            if (dirFromAttr) {
+                dir = /[trbl]{2}/.test(dirFromAttr) ? dirFromAttr : '1';
+            }
+
+            var second, first;
+
+            // 未指定方向时自动按下右上左顺序计算可用方向（以不超出视窗为原则）
+            if (dir === 'auto') {
+
+                // 目标宽度大于提示层宽度时优先考虑水平居中
+                var horiz = width > mainWidth
+                        || left - (mainWidth - width) / 2 > 0
+                        && right + (mainWidth - width) / 2 <= scrollRight
+                        ? 'c'
+                        : left + mainWidth > scrollRight
+                            ? 'r'
+                            : 'l';
+
+                // 目标高度大于提示层高度时优先考虑垂直居中
+                var vertical = height > mainHeight
+                        || top - (mainHeight - height) / 2 > 0
+                        && bottom + (mainHeight - height) / 2 <= scrollBottom
+                        ? 'c'
+                        : top + mainHeight > scrollBottom
+                            ? 'b'
+                            : 't';
+
+                // 如果提示层在目标下边未超出视窗
+                if (bottom + mainHeight <= scrollBottom) {
+                    first = 'b';
+                    second = horiz;
+                }
+
+                // 如果提示层在目标右侧未超出视窗
+                else if (right + mainWidth <= scrollRight) {
+                    first = 'r';
+                    second = vertical;
+                }
+
+                // 如果提示层在目标上边未超出视窗
+                else if (top - mainHeight >= scrollTop) {
+                    first = 't';
+                    second = horiz;
+                }
+
+                // 如果提示层在目标左侧未超出视窗
+                else if (left - mainWidth >= scrollLeft) {
+                    first = 'l';
+                    second = vertical;
+                }
+
+                dir = first + second;
+            }
+            else {
+
+                // 从 dir 中分拆水平和垂直方向值，方便后续计算
+                first = dir.charAt(0);
+                second = dir.charAt(1);
+            }
+
+            var offset = options.offset;
+
+            // 提示层在目标上部或下部显示时的定位处理
+            if ({t: 1, b: 1}[first]) {
+                left = {
+                    l: left,
+                    c: center - (mainWidth / 2),
+                    r: right - mainWidth
+                }[second];
+
+                top = {
+                    t: top - mainHeight,
+                    b: bottom
+                }[first];
+
+            }
+
+            // 提示层在目标左边或右边显示时的定位处理
+            else if ({l: 1, r: 1}[first]) {
+                top = {
+                    t: top,
+                    c: middle - (mainHeight / 2),
+                    b: bottom - mainHeight
+                }[second];
+
+                left = {
+                    l: left - mainWidth - offset.x,
+                    r: right + offset.x
+                }[first];
+
+            }
+
+            DOM.setStyles(main, {left: left + offset.x, top: top + offset.y});
+
+        }
+
+    };
+    T.inherits(Popup, Control);
+
+    return Popup;
+});
---
+++ src/ui/log.js
@@ -0,0 +1,485 @@
+/**
+ * ZXUI (Zhixin UI)
+ * Copyright 2013 Baidu Inc. All rights reserved.
+ * 
+ * @file  知心中间页日志统计模块
+ * @author  chris(wfsr@foxmail.com)
+ */
+
+define(function (require) {
+    var T = baidu;
+    var DOM = T.dom;
+    var parseJson = T.json.parse;
+
+    require('./Control');
+
+    /**
+     * 发送日志请求
+     * 
+     * @inner
+     * @param {string} url 日志完整地址
+     * @fires module:log#send
+     */
+    var send = (function () {
+        var list = [];
+        var encode = function (value) {
+            return encodeURIComponent(value);
+        };
+
+        return function (data) {
+            var index = list.push(new Image()) - 1;
+
+            list[index].onload = list[index].onerror = function () {
+                list[index] = list[index].onload = list[index].onerror = null;
+                delete list[index];
+            };
+
+            var url = options.action + T.url.jsonToQuery(data, encode);
+
+            list[index].src = url;
+
+            /**
+             * @event module:log#send
+             * @type {Object}
+             * @property {string} url 当前统计请求的完整地址
+             */
+            exports.fire('send', {url: url});
+        };
+    })();
+
+    /**
+     * 填充数据
+     * 根据当前点击对象，解释对象所处 XPath 及 url
+     * 
+     * @inner
+     * @param {Object} data待发送的数据对象
+     * @param {HTMLElement} from 当前点击对象
+     * @param {HTMLElement} to 统计日志最上层容器
+     * @return {Object} 合并所有HTML自定义属性和相关配置项后的数据对象
+     */
+    var fill = function (data, from, to) {
+        var type;
+        var url;
+        var nolog = 0;
+        var el = from;
+        var path = [];
+
+        var i = 0;
+        var clickData;
+        while (el !== to) {
+            if (el.getAttribute('data-nolog') === '1') {
+                nolog = 1;
+                break;
+            }
+
+            clickData = el.getAttribute('data-click');
+            if (clickData) {
+                data = T.extend(parseJson(clickData), data);
+            }
+
+            if (el.href) {
+                url = el.href;
+                type = 'link';
+            }
+
+            if (type === 'link' && el.tagName === 'H3') {
+                type = 'title';
+            }
+
+            var count = 1;
+            if (el.previousSibling) {
+                var sibling = el.previousSibling;
+
+                do {
+                    if (sibling.nodeType === 1
+                        && sibling.tagName === el.tagName
+                    ) {
+                        count++;
+                    }
+                    sibling = sibling.previousSibling;
+                } while (sibling);
+            }
+
+            path[i++] = el.tagName + (count > 1 ? count : '');
+
+            el = el.parentNode;
+
+        }
+
+        if (from !== to) {
+            clickData = to.getAttribute('data-click');
+            if (clickData) {
+                data = T.extend(parseJson(clickData), data);
+            }            
+        }
+
+        if (nolog) {
+            return !nolog;
+        }
+
+        // 反转 XPath 顺序
+        path.reverse();
+        var tag = from.tagName.toLowerCase();
+
+        if (!type) {
+
+            var typeReg = /\bOP_LOG_(TITLE|LINK|IMG|BTN|INPUT|OTHERS)\b/i;
+            if (typeReg.test(from.className)) {
+                type = RegExp.$1.toLowerCase();
+            }
+            else if (
+                /^a|img|input|button|select|datalist|textarea$/.test(tag)
+            ) {
+                type = {a: 'link', button: 'btn'}[tag] || 'input';
+
+                url = from.href || from.src || url;
+            }
+            else {
+                T.each(
+                    'title,link,img,btn,input,others'.split(','),
+                    function (key) {
+                        if (DOM.hasClass(from, options[key])) {
+                            type = key;
+                        }
+                    }
+                );
+            }
+
+        }
+
+        if (!type) {
+            return false;
+        }
+
+        var mainUrl = to.getAttribute('mu') || '';
+        data.mu = mainUrl;
+        if (type === 'title') {
+            delete data.mu;
+        }
+
+        if (!url || url.slice(-1) === '#') {
+            url = mainUrl;
+        }
+
+        if (url) {
+            data.url = url;
+        }
+
+
+        var title = '';
+
+        // 如果是表单元素
+        if (type === 'input') {
+            if (/input|textarea/.test(tag)) {
+                title = from.value;
+                if (from.type && from.type.toLowerCase() === 'password') {
+                    title = '';
+                }
+            }
+            else if (/select|datalist/.test(tag)) {
+                if (from.children.length > 0) {
+                    var index = from.selectedIndex || 0;
+                    title = from.children[index > -1 ? index : 0].innerHTML;
+                }
+            }
+            else {
+                title = from.innerHTML || from.value || '';
+            }
+        }
+        else {
+
+            // 如果是图片，先取其title
+            if (tag === 'img') {
+                title = from.title;
+            }
+
+            // title为空，遍历父节点
+            if (!title) {
+                while (i > 1) {
+                    i--;
+                    if (/^a\d*\b/.test(path[i])) {
+                        url = el.href;
+                        title = el.innerHTML;
+                        break;
+                    }
+                    else {
+                        if(el.className
+                            && (/\bOP_LOG_[A-Z]+\b/.test(el.className))
+                        ){
+                            title = el.innerHTML;   
+                            break;
+                        }
+                        el = el.parentNode;
+                    }
+                }
+            }
+        }
+        data.title = T.trim(title);
+
+        data['rsv_xpath'] = path.join('-').toLowerCase() + '(' + type + ')';
+        data['rsv_height'] = to.offsetHeight;
+        data.path = location.href;
+        return data;
+    };
+
+    /**
+     * 配置项
+     * 
+     * @type {Object}
+     */
+    var options = {
+
+        /**
+         * 日志统计服务接口地址
+         * 
+         * @type {string}
+         */ 
+        action: 'http://sclick.baidu.com/w.gif?',
+
+        /**
+         * 日志统计顶层容器 className
+         * 
+         * @type {string}
+         */ 
+        main: 'result-op',
+
+        /**
+         * xpath 中 title 类型的 className
+         * 
+         * @type {string}
+         */ 
+        title: 'OP_LOG_TITLE',
+
+        /**
+         * xpath 中 link 类型的 className
+         * 
+         * @type {string}
+         */ 
+        link: 'OP_LOG_LINK',
+
+        /**
+         * xpath 中 img 类型的 className
+         * 
+         * @type {string}
+         */ 
+        img: 'OP_LOG_IMG',
+
+        /**
+         * xpath 中 btn 类型的 className
+         * 
+         * @type {string}
+         */ 
+        btn: 'OP_LOG_BTN',
+
+        /**
+         * xpath 中 input  类型的 className
+         * 
+         * @type {string}
+         */ 
+        input: 'OP_LOG_INPUT',
+
+        /**
+         * xpath 中 others 类型的 className
+         * 
+         * @type {string}
+         */ 
+        others: 'OP_LOG_OTHERS',
+
+        /**
+         * 统计公共数据部分
+         * 
+         * 中间页 p1 永远为 1
+         * @type {string}
+         */ 
+        data: {
+            p1: 1
+        }
+    };
+
+    /**
+     * 绑定 P5 参数索引值
+     * 
+     * @param {[type]} el [el description]
+     * @param {[type]} index [index description]
+     * @return {[type]} [return description]
+     */
+    var bindP5 = function (el, index) {
+        var data = el.getAttribute('data-click') || '{}';
+        try {
+            data = parseJson(data);
+        }
+        catch (e) {
+            data = {};
+        }
+
+        data.p5 = index;
+        el.setAttribute('data-click', T.json.stringify(data));
+    };
+
+    /**
+     * 页面点击监听
+     * http://fe.baidu.com/doc/aladdin/#standard/aladdin_click.text
+     * 
+     * @inner
+     * @param {?DOMEvent} e DOM 事件对象
+     * @param {HTMLElement=} el 指定触发统计的 HTMLElement
+     * @fires module:log#click
+     */
+    var onClick = function (e, el) {
+        var target = el || T.event.getTarget(e);
+        var klass = options.main;
+        var nolog = target.getAttribute('data-nolog') === '1';
+        var main = DOM.hasClass(target, klass)
+            ? target
+            : DOM.getAncestorByClass(target, klass);
+
+        if (nolog || !main || main.getAttribute('data-nolog') === '1') {
+            return;
+        }
+
+        var data = target.getAttribute('data-click');
+
+        if (data) {
+            data = parseJson(data);
+        }
+
+        data = fill(data || {}, target, main);
+
+        // 某个上级节点配置了 data-nolog 之后
+        if (!data) {
+            return;
+        }
+
+        if (options.data) {
+            data = T.extend(T.extend({}, options.data), data);
+        }
+
+        // 仅当首次点击或有新加入节点时计算 p5 序号值
+        if (!('p5' in data)) {
+            T.each(
+                T.q(options.main),
+                function (el, i) {
+                    if (el === main) {
+                        data.p5 = i + 1;
+                    }
+                    bindP5(el, i + 1);
+                }
+            );
+        }
+
+        data.t = (+new Date()).toString(36);
+
+        /**
+         * @event module:log#click
+         * @type {Object}
+         * @property {Object} data 上报的公共数据
+         * @property {string} data.rsv_xpath 事件源 DOM 节点的XPath(type)
+         * @property {number} data.p5 当前统计区域（卡片）的索引值
+         * @property {string} data.type 统计类型
+         * @property {string} data.t 时间截的 36 进制表示
+         * @property {string} target 点击事件源对象
+         * @property {string} main 当前统计区域（卡片）主容器
+         */
+        exports.fire('click', {data: data, target: target, main: main});
+
+        send(data);
+    };
+
+    /**
+     * 中间页日志统计模块
+     * 
+     * @module log
+     * @example
+     * log.config({action: 'http://www.domain.com/api'});
+     * log.start();
+     */
+    var exports = {
+
+        /**
+         * 配置项
+         * 
+         * @see options
+         * @param {Object} ops 可配置项
+         * @param {string=} ops.action 日志统计服务接口地址
+         * @param {string=} ops.main 日志统计顶层容器 className
+         * @param {string=} ops.title xpath 中 title 类型的 className
+         * @param {string=} ops.link xpath 中 link 类型的 className
+         * @param {string=} ops.img xpath 中 img 类型的 className
+         * @param {string=} ops.btn xpath 中 btn 类型的 className
+         * @param {string=} ops.input xpath 中 input 类型的 className
+         * @param {string=} ops.others xpath 中 others 类型的 className
+         * @param {Object=} ops.data 统计公共数据部分
+         */
+        config: function (ops) {
+            T.extend(options, ops);
+        },
+
+        /**
+         * 开始监听页面点击日志
+         * 
+         */
+        start: function () {
+            T.on(document, 'click', onClick);
+        },
+
+        /**
+         * 停止监听页面点击日志
+         * 
+         */
+        stop: function () {
+            T.un(document, 'click', onClick);
+        },
+
+        /**
+         * 模拟点击指定的 HTMLElement 以发送统计
+         * 
+         * @param {HTMLElement} el 需要模拟点击触发统计的 HTMLElement
+         */
+        click: function (el) {
+            onClick(null, el);
+        },
+
+        /**
+         * 手动发送统计请求
+         * 
+         * @param {Object} data 要发送的数据
+         */
+        send: send
+    };
+
+    var observe = T.lang.Class.prototype;
+    T.extend(exports, {
+
+        /**
+         * 添加事件绑定
+         * 
+         * @static
+         * @method module:log.on
+         * @param {?string} type 事件类型
+         * @param {Function} listner 要添加绑定的监听器
+         */
+        on: observe.addEventListener,
+
+        /**
+         * 解除事件绑定
+         * 
+         * @static
+         * @method module:log.un
+         * @param {string=} type 事件类型
+         * @param {Function=} listner 要解除绑定的监听器
+         */
+        un: observe.removeEventListener,
+
+        /**
+         * 触发指定事件
+         * 
+         * @static
+         * @method module:log.fire
+         * @param {string} type 事件类型
+         * @param {Object} args 透传的事件数据对象
+         */
+        fire: observe.dispatchEvent
+    });
+
+    return exports;
+});
